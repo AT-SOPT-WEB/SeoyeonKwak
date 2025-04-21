94ccc2a7ca2b3ef30f50db96ba86121fc286a6ec
yeonilil <gwagseoyeon@gmail.com>
2025-04-21 20:03:03 +0900

    feat: 로컬 스토리지 데이터 테이블로 렌더링 구현

--- week2-homework/index.js
+++ week2-homework/index.js
@@ -2,4 +2,37 @@ fetch("data.json")
   .then((response) => response.json())
   .then((data) => {
     localStorage.setItem("todos", JSON.stringify(data));
+    renderTable(data);
   });
+
+function renderTable(todos) {
+  const tbody = document.querySelector("#todo-table");
+  tbody.innerHTML = "";
+
+  todos.forEach((todo) => {
+    const tr = document.createElement("tr");
+
+    const tdCheck = document.createElement("td");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = todo.completed;
+    tdCheck.appendChild(checkbox);
+
+    const tdPriority = document.createElement("td");
+    tdPriority.textContent = todo.priority;
+
+    const tdCompleted = document.createElement("td");
+    tdCompleted.textContent = todo.completed ? "완료" : "미완료";
+
+    const tdTitle = document.createElement("td");
+    tdTitle.textContent = todo.title;
+
+    // tr에 td들 추가
+    tr.appendChild(tdCheck);
+    tr.appendChild(tdPriority);
+    tr.appendChild(tdCompleted);
+    tr.appendChild(tdTitle);
+
+    tbody.appendChild(tr);
+  });
+}
