6d1a7a483e579e78f79b5f6e6db1fc380e70ce5f
yeonilil <gwagseoyeon@gmail.com>
2025-05-01 13:52:45 +0900

    feat: 3주차 실습

--- week2-react/src/App.jsx
+++ week2-react/src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from "react";
 import Card from "./components/Card";
 import Header from "./components/Header";
 import Search from "./components/Search";
 
 import { members } from "./data/member";
+import useSearch from "./hooks/useSearch";
 
 function App() {
-  const [searchContent, setSearchContent] = useState("");
+  const { searchContent, filteredMembers, handleSearchChange, handleSearch } =
+    useSearch(members);
 
-  const handleSearch = (event) => {
-    setSearchContent(event.target.value);
-    console.log(event.target.value);
-  };
   return (
     <>
       <Header />
-      <Search searchContent={searchContent} handleSearch={handleSearch} />
+      <Search
+        searchContent={searchContent}
+        handleSearch={handleSearch}
+        handleSearchChange={handleSearchChange}
+      />
       <section style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
-        {members
-          .filter((member) =>
-            member.name.toLowerCase().includes(searchContent.toLowerCase())
-          )
-          .map((member) => (
-            <Card
-              key={member.id}
-              name={member.name}
-              github={member.github}
-              englishName={member.englishName}
-            />
-          ))}
+        {filteredMembers.map((member) => (
+          <Card
+            key={member.id}
+            name={member.name}
+            github={member.github}
+            englishName={member.englishName}
+          />
+        ))}
       </section>
     </>
   );
